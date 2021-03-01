4f6eb4c1dd7a7b771e21024fadd80ff747aed4e7
DronCorleone <48313301+DronCorleone@users.noreply.github.com>
2021-03-01 22:55:33 +0300

    Initial commit

---
+++ Assets/Scripts/BaseBomb.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public abstract class BaseBomb : MonoBehaviour
+{
+    private int _damage;
+    private int _radius;
+
+
+    protected abstract void SetPower();
+    protected abstract void Explode();
+}
---
+++ Assets/Scripts/BaseCharacter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public abstract class BaseCharacter : MonoBehaviour
+{
+    private int _hp;
+
+
+    protected abstract void SetStats();
+    protected abstract void GetDamage(int damage);
+}
